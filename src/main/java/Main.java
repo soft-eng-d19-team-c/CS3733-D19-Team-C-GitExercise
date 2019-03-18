85b3a48190d2ac119bccb8ba2885f4eeeb167c64
Jacob Tutlis <jtutlis@gmail.com>
2019-03-17 20:45:38 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Ryan LaMarche");
         System.out.println("Lara Padir");
-<<<<<<< HEAD
         System.out.println("Kyle");
-=======
         System.out.println("Jacob Tutlis");
->>>>>>> 3af6a7dd26d2bcaa8457a19f4cb415a5bd711149
     }
 }
